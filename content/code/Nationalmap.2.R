ba5169a324c88ff2283e8238409313b1b6756415
Isabella Garfield <isabellagarfield@gmail.com>
2025-01-29 14:35:08 -0500

    Adding maps of regional centers

--- content/code/Nationalmap.2.R
+++ content/code/Nationalmap.2.R
@@ -19,41 +19,51 @@ library(spDataLarge)
 library(ggplot2)
 library(fiftystater)
 library(mapproj)
+library(gridExtra)
+library(grid)
+library(png)
 
 regional_offices <- read.csv("content/code/Regional_offices_mainland.csv")
 geocode <- st_as_sf(regional_offices, coords = c("Long", "Lat"), crs = 4326)
 
 mainland <- tm_shape(us_states) +
   tm_fill() +
-#  tm_polygons(alpha = 0.7, legend.show = FALSE) +
   tm_shape(geocode) +
-  tm_bubbles(col = "black", fill = "Regional_office", size = .5) +
-  tm_text("Name1", size = 0.7, ymod = 1, xmod = 1, col = "coral3") +
-  tm_text("Name2", size = 0.6, ymod = -.8, xmod = -.6) +
-  tm_text("Name3", size = 0.6, ymod = .6, xmod = -2) +
-  tm_text("Name4", size = 0.6, ymod = -1, xmod = 1) +
-  tm_text("Name5", size = 0.7, ymod = -1, xmod = 1.2, col = "blue") +
-  tm_text("Name6", size = 0.7, ymod = 0, xmod = 2.7, col = "darkgoldenrod") +
+  tm_polygons(legend.show = F) +
+  tm_bubbles(col = "black", fill = "Office", size = .5) +
+  tm_text("Name1", size = 0.7, ymod = -.5, xmod = 1.9, col = "coral3") +
+  tm_text("Name2", size = 0.6, ymod = 0, xmod = -1.9) +
+  tm_text("Name3", size = 0.6, ymod = 0, xmod = -1.7) +
+  tm_text("Name4", size = 0.6, ymod = 0.8, xmod = -1.3) +
+  tm_text("Name5", size = 0.7, ymod = 0, xmod = 3, col = "blue") +
+  tm_text("Name6", size = 0.7, ymod = 0, xmod = 2.4, col = "darkgoldenrod") +
   tm_text("Name7", size = 0.6, ymod = -1, xmod = 1.2) +
   tm_text("Name8", size = 0.6, ymod = -.7, xmod = 0) +
   tm_text("Name9", size = 0.6, ymod = 1.2, xmod = 0) +
-  tm_text("Name10", size = 0.6, ymod = 0, xmod = 4.4) +
+  tm_text("Name10", size = 0.6, ymod = 0, xmod = 4) +
   tm_text("Name11", size = 0.7, ymod = -1, xmod = 0, col = "darkgreen") +
-  tm_text("Name12", size = 0.6, ymod = 1, xmod = 1.2) +
-  tm_text("Name13", size = 0.6, ymod = -.8, xmod = 1.2) 
+  tm_text("Name12", size = 0.6, ymod = .8, xmod = 3) +
+  tm_text("Name13", size = 0.6, ymod = -.6, xmod = 3) 
 
 mainland
+tmap_save(mainland, "content/code/output/Mainland.png", dpi = 900)
 
+#### Alaska  ####
 regional_offices.alaska <- read.csv("content/code/Regional_offices_alaska.csv")
 geocode.alaska <- st_as_sf(regional_offices.alaska, coords = c("Long", "Lat"), crs = 4326)
 
 alaska <- tm_shape(alaska) +
   tm_fill() +
   tm_shape(geocode.alaska) +
-  tm_bubbles(col = "black", fill = "Regional_office", size = .5) +
-  tm_text("Name1", size = 0.7, ymod = 1, xmod = 1, col = "darkorchid4") +
-  tm_text("Name2", size = 0.6, ymod = -.8, xmod = -.6) 
+  tm_bubbles(col = "black", fill = "darkorchid4", size = .5) +
+  tm_text("Name1", size = 1.3, ymod = 1, xmod = 0, col = "darkorchid4") +
+  tm_text("Name2", size = 1.3, ymod = -.8, xmod = -.6) 
+
 alaska
+tmap_save(alaska, "content/code/output/Alaska.png", dpi = 900)
+
+
+#### Hawaii ####
 
 regional_offices.hawaii <- read.csv("content/code/Regional_offices_hawaii.csv")
 geocode.hawaii <- st_as_sf(regional_offices.hawaii, coords = c("Long", "Lat"), crs = 4326)
@@ -62,11 +72,16 @@ hawaii <- tm_shape(hawaii) +
   tm_fill() +
   tm_borders() +
   tm_shape(geocode.hawaii) +
-  tm_bubbles(col = "black", fill = "Regional_office", size = .5) +
-  tm_text("Name", size = 0.7, ymod = 1, xmod = 1, col = "cyan4")
+  tm_bubbles(col = "black", fill = "cyan4", size = .5) +
+  tm_text("Name", size = 1.3, ymod = 0, xmod = -5, col = "cyan4") +
+  tm_add_legend(type = "fill", 
+                labels = c("AFSC", "PIFSC"),
+                col = c("darkorchid4", "cyan4"),
+                border.lwd = 0.5,
+                title = "Office",
+                is.portrait = F)
 
-
-mainland
 hawaii
-alaska
+tmap_save(hawaii, "content/code/output/Hawaii.png", dpi = 900)
 
+       
