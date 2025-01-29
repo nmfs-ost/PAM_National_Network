ca4251ff6322909b1d8b21c9bd00548c655b9203
Isabella Garfield <isabellagarfield@gmail.com>
2025-01-29 15:55:25 -0500

    Map update

--- content/code/Nationalmap.2.R
+++ content/code/Nationalmap.2.R
@@ -31,19 +31,19 @@ mainland <- tm_shape(us_states) +
   tm_shape(geocode) +
   tm_polygons(legend.show = F) +
   tm_bubbles(col = "black", fill = "Office", size = .5) +
-  tm_text("Name1", size = 0.7, ymod = -.5, xmod = 1.9, col = "coral3") +
+  tm_text("Name1", size = 0.8, ymod = 0, xmod = 2, col = "coral3") +
   tm_text("Name2", size = 0.6, ymod = 0, xmod = -1.9) +
   tm_text("Name3", size = 0.6, ymod = 0, xmod = -1.7) +
   tm_text("Name4", size = 0.6, ymod = 0.8, xmod = -1.3) +
-  tm_text("Name5", size = 0.7, ymod = 0, xmod = 3, col = "blue") +
-  tm_text("Name6", size = 0.7, ymod = 0, xmod = 2.4, col = "darkgoldenrod") +
-  tm_text("Name7", size = 0.6, ymod = -1, xmod = 1.2) +
-  tm_text("Name8", size = 0.6, ymod = -.7, xmod = 0) +
-  tm_text("Name9", size = 0.6, ymod = 1.2, xmod = 0) +
-  tm_text("Name10", size = 0.6, ymod = 0, xmod = 4) +
-  tm_text("Name11", size = 0.7, ymod = -1, xmod = 0, col = "darkgreen") +
-  tm_text("Name12", size = 0.6, ymod = .8, xmod = 3) +
-  tm_text("Name13", size = 0.6, ymod = -.6, xmod = 3) 
+  tm_text("Name5", size = 0.8, ymod = 0, xmod = 3, col = "deepskyblue4") +
+  tm_text("Name6", size = 0.8, ymod = 0, xmod = 2.4, col = "darkgoldenrod") +
+  tm_text("Name7", size = 0.8, ymod = 0, xmod = 3) +
+  tm_text("Name8", size = 0.8, ymod = -.7, xmod = 0) +
+  tm_text("Name9", size = 0.8, ymod = 1, xmod = 0) +
+  tm_text("Name10", size = 0.8, ymod = 0, xmod = 4) +
+  tm_text("Name11", size = 0.8, ymod = 0, xmod = 3, col = "darkgreen") +
+  tm_text("Name12", size = 0.8, ymod = .8, xmod = 3) +
+  tm_text("Name13", size = 0.8, ymod = -.6, xmod = 3) 
 
 mainland
 tmap_save(mainland, "content/code/output/Mainland.png", dpi = 900)
@@ -55,7 +55,7 @@ geocode.alaska <- st_as_sf(regional_offices.alaska, coords = c("Long", "Lat"), c
 alaska <- tm_shape(alaska) +
   tm_fill() +
   tm_shape(geocode.alaska) +
-  tm_bubbles(col = "black", fill = "darkorchid4", size = 1) +
+  tm_bubbles(col = "black", fill = "darkorchid3", size = 1) +
   tm_text("Name1", size = 1.3, ymod = 1, xmod = 0, col = "darkorchid4") +
   tm_text("Name2", size = 1.3, ymod = -.8, xmod = -.6) 
 
@@ -72,11 +72,11 @@ hawaii <- tm_shape(hawaii) +
   tm_fill() +
   tm_borders() +
   tm_shape(geocode.hawaii) +
-  tm_bubbles(col = "black", fill = "cyan4", size = 1) +
-  tm_text("Name", size = 1.3, ymod = 0, xmod = -5, col = "cyan4") 
+  tm_bubbles(col = "black", fill = "cyan3", size = 1) +
+  tm_text("Name", size = 1.3, ymod = 0, xmod = -5, col = "cyan4") +
   tm_add_legend(type = "fill", 
-                labels = c("AFSC", "PIFSC"),
-                col = c("coral3" "darkorchid4", "cyan4"),
+                labels = c("NEFSC", "SEFSC", "NWFSC", "SWFSC", "AFSC", "PIFSC"),
+                col = c("coral2","darkgoldenrod1","deepskyblue4","chartreuse3","darkorchid3","cyan3"),
                 border.lwd = 0.5,
                 title = "Office",
                 is.portrait = F)
